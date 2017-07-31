7e437420745bdf856509de02ad3a3ad6b5386d64
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 13:57:00 +0300

    add keyboard

--- index.php
+++ index.php
@@ -10,8 +10,36 @@ $output = json_decode(file_get_contents('php://input'), true);
 file_put_contents('logs.txt', $output);
 $id = $output['message']['chat']['id'];
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
-sendMessage($token, $id);
-function sendMessage($token, $id){
-    file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=i can see");
+$message = $output['message']['text'];
+
+switch ($message){
+    case 'hi':
+        $message = 'Hello';
+        sendMessage($token, $id, $message);
+        break;
+    case 'How are you':
+        $message = 'I am find';
+        sendMessage($token, $id, $message.KeyboardMenu());
+        break;
+    default:
+        $message = 'What are you say';
+        sendMessage($token, $id, $message);
+}
+
+function sendMessage($token, $id, $message){
+    file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=".$message);
 }
 file_put_contents('logs.txt', $id);
+
+function KeyboardMenu(){
+    $buttons = [['hi'], ['how are you']];
+    $keyboard = json_encode($keyboard = [
+        'keyboard' => $buttons,
+        'resize_keyboard' => false,
+        'one_time_keyboard' => false,
+        'selective' => false,
+    ]);
+
+    $replayKey = '$replay_markup'.$keyboard;
+    return $replayKey;
+}
