c714204b0c5ea8516c596e8e1e44e1b2b82bdb8e
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 11:51:10 +0300

    add json_decode for file logs.txt

--- index.php
+++ index.php
@@ -6,5 +6,5 @@
  * Time: 10:35
  */
 
-$output = json_decode(file_get_contents('php://input'));
+$output = json_decode(file_get_contents('php://input'), true);
 file_put_contents('logs.txt', $output);
