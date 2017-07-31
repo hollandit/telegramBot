2d156f574548c32da7c0e23c86f5dc1d280cf4a8
Руслан Абсалямов <holland.itkzn@gmail.com>
2017-07-31 13:23:01 +0300

    add function sendMessage

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $output = json_decode(file_get_contents('php://input'), true);
 file_put_contents('logs.txt', $output);
 $id = $output['message']['chat']['id'];
 $token = '414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0';
+sendMessage($token, $id);
 function sendMessage($token, $id){
     file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=i can see");
 }
