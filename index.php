c3a2c65ead453b8f910abe358961b3030a29e59c
hollandit <holland.itkzn@gmail.com>
2017-07-31 12:25:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,5 @@
 
 $output = json_decode(file_get_contents('php://input'), true);
 $id = $output['message']['chat']['id'];
-file_get_contents("https://api.telegram.org/bot414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0/sendMessage?chat_id=".$id."&text=i can see")
+file_get_contents("https://api.telegram.org/bot414134665:AAHfOIdeikQD04NdKckL8wadhqzggvmSqw0/sendMessage?chat_id=".$id."&text=i can see");
 file_put_contents('logs.txt', $id);
